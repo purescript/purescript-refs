b57119b6b2d44789bd95f154fdaf2a8be507f2b5
Michael Xavier <michael@michaelxavier.net>
2014-11-21 14:34:23 -0800

    Add modifyRef that returns a pure value
    This is equivalent to the atomicModifyIORef from Haskell

--- src/Control/Monad/Eff/Ref.purs
+++ src/Control/Monad/Eff/Ref.purs
@@ -1,42 +1,51 @@
 module Control.Monad.Eff.Ref where
 
 import Control.Monad.Eff
+import Data.Tuple
 
 foreign import data Ref :: !
 
 foreign import data RefVal :: * -> *
 
-foreign import newRef 
-  "function newRef(val) {\
-  \  return function () {\
-  \    return { value: val };\
-  \  };\
-  \}" :: forall s r. s -> Eff (ref :: Ref | r) (RefVal s)
+foreign import newRef """
+  function newRef(val) {
+    return function () {
+      return { value: val };
+    };
+  }
+""" :: forall s r. s -> Eff (ref :: Ref | r) (RefVal s)
 
-foreign import readRef 
-  "function readRef(ref) {\
-  \  return function() {\
-  \    return ref.value;\
-  \  };\
-  \}" :: forall s r. RefVal s -> Eff (ref :: Ref | r) s
+foreign import readRef """
+  function readRef(ref) {
+    return function() {
+      return ref.value;
+    };
+  }
+""" :: forall s r. RefVal s -> Eff (ref :: Ref | r) s
 
 
-foreign import modifyRef 
-  "function modifyRef(ref) {\
-  \  return function(f) {\
-  \    return function() {\
-  \      ref.value = f(ref.value);\
-  \      return {};\
-  \    };\
-  \  };\
-  \}" :: forall s r. RefVal s -> (s -> s) -> Eff (ref :: Ref | r) Unit
+foreign import modifyRef' """
+  function modifyRef$prime(ref) {
+    return function(f) {
+      return function() {
+        var t = f(ref.value);
+        ref.value = t.value0;
+        return t.value1;
+      };
+    };
+  }
+""" :: forall s b r. RefVal s -> (s -> Tuple s b) -> Eff (ref :: Ref | r) b
 
-foreign import writeRef 
-  "function writeRef(ref) {\
-  \  return function(val) {\
-  \    return function() {\
-  \      ref.value = val;\
-  \      return {};\
-  \    };\
-  \  };\
-  \}" :: forall s r. RefVal s -> s -> Eff (ref :: Ref | r) Unit
+modifyRef :: forall s r. RefVal s -> (s -> s) -> Eff (ref :: Ref | r) Unit
+modifyRef ref f = modifyRef' ref (\s -> Tuple (f s) unit)
+
+foreign import writeRef """
+  function writeRef(ref) {
+    return function(val) {
+      return function() {
+        ref.value = val;
+        return {};
+      };
+    };
+  }
+""" :: forall s r. RefVal s -> s -> Eff (ref :: Ref | r) Unit
