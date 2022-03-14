dedc714f0d2de6fc4a3c0cfb91475b931e388db3
JordanMartinez <jordanalex.martinez@protonmail.com>
2022-03-14 09:32:29 -0700

    Update to v0.15.0 (#39)
    * Convert foreign modules to try bundling with esbuild
    
    * Replaced 'export var' with 'export const'
    
    * Removed '"use strict";' in FFI files
    
    * Update to CI to use 'unstable' purescript
    
    * Update pulp to 16.0.0-0 and psa to 0.8.2
    
    * Update Bower dependencies to master
    
    * Update .eslintrc.json to ES6
    
    * Added changelog entry
    
    Co-authored-by: Cyril Sobierajewicz <sobierajewicz.cyril@gmail.com>

--- src/Effect/Ref.js
+++ src/Effect/Ref.js
@@ -1,12 +1,10 @@
-"use strict";
-
-exports.new = function (val) {
+export const _new = function (val) {
   return function () {
     return { value: val };
   };
 };
 
-exports.newWithSelf = function (f) {
+export const newWithSelf = function (f) {
   return function () {
     var ref = { value: null };
     ref.value = f(ref);
@@ -14,13 +12,13 @@ exports.newWithSelf = function (f) {
   };
 };
 
-exports.read = function (ref) {
+export const read = function (ref) {
   return function () {
     return ref.value;
   };
 };
 
-exports.modifyImpl = function (f) {
+export const modifyImpl = function (f) {
   return function (ref) {
     return function () {
       var t = f(ref.value);
@@ -30,7 +28,7 @@ exports.modifyImpl = function (f) {
   };
 };
 
-exports.write = function (val) {
+export const write = function (val) {
   return function (ref) {
     return function () {
       ref.value = val;
